27313e3bb4542c5e865553f1a23174ce09328f76
David Domkář <dave.domkar@gmail.com>
2019-05-19 11:32:43 +0200

    Added basic iOS support

--- ios/Classes/SwiftFlutterSystemBarsPlugin.swift
+++ ios/Classes/SwiftFlutterSystemBarsPlugin.swift
@@ -2,13 +2,50 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterSystemBarsPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_system_bars", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterSystemBarsPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_system_bars", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterSystemBarsPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+            case "hasSoftwareNavigationBar":
+                hasSoftKeys(result: result)
+            case "softwareNavigationBarPhysicalHeight":
+                getNavBarHeight(result: result)
+            case "statusBarPhysicalHeight":
+                getStatusBarHeight(result: result)
+            default:
+                result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func hasSoftKeys(result: FlutterResult) {
+        result(Bool(false))
+    }
+
+    private func getNavBarHeight(result: FlutterResult) {
+        if #available(iOS 11.0, *) {
+            if (UIApplication.shared.delegate != nil && UIApplication.shared.delegate!.window != nil) {
+                result(Int(UIApplication.shared.delegate!.window!!.safeAreaInsets.bottom * UIScreen.main.scale))
+            } else {
+                result(Int(0))
+            }
+        } else {
+            result(Int(0))
+        }
+    }
+
+    private func getStatusBarHeight(result: FlutterResult) {
+        if #available(iOS 11.0, *) {
+            if (UIApplication.shared.delegate != nil && UIApplication.shared.delegate!.window != nil) {
+                result(Int(UIApplication.shared.delegate!.window!!.safeAreaInsets.top * UIScreen.main.scale))
+            } else {
+                result(Int(0))
+            }
+        } else {
+            result(Int(0))
+        }
+    }
 }
